d22981f8c467f0a2bf3e7a12a9d58af16092031a
Marko Vračaričić <marko.vracaricic.83@gmail.com>
2024-10-09 04:41:19 +0200

    removing empty rows js-script

--- script.js
+++ script.js
@@ -1,41 +1,26 @@
-
-
 const toggler = document.querySelector('.navbar-toggler');
 const callNumber = document.querySelector('.disappear-large');
 const navbar = document.querySelector('.navbar-collapse');
 var switcher = true;
-
-
-
-
-
-
-
 var map = L.map('map').setView([44.8189, 20.4559], 15); // Promenjene koordinate na Dorćol, Beograd
-
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
   maxZoom: 18,
   scrollWheelZoom: false,
 }).addTo(map);
-
 // Dodavanje markera na Dorćol
 L.marker([44.8189, 20.4559]) 
   .addTo(map)
   .bindPopup('We are here.') 
   .openPopup();
-
-
 toggler.addEventListener('click', () => {
   if (switcher) {
     callNumber.classList.add('margin-auto');
   } else {
     setTimeout(()=>{callNumber.classList.remove('margin-auto');},300);
-    
   }
   switcher = !switcher;
 });
-
 document.querySelectorAll('.nav-link').forEach((link) => {
   link.addEventListener('click', () => {
     if (navbar.classList.contains('show')) {
@@ -48,9 +33,6 @@ document.querySelectorAll('.nav-link').forEach((link) => {
     switcher = true;
   });
 });
-
-
 window.addEventListener('load', () => {
   window.scrollTo(0, 0); 
-  
 });
