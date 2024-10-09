eb872542eeeb57d44eb65bcb2895afb236279358
Marko Vračaričić <marko.vracaricic.83@gmail.com>
2024-10-09 04:37:02 +0200

    two fingers for map

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ var map = L.map('map').setView([44.8189, 20.4559], 15); // Promenjene koordinate
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
   maxZoom: 18,
+  scrollWheelZoom: false,
 }).addTo(map);
 
 // Dodavanje markera na Dorćol
