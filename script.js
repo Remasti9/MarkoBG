d1040953aaf1b2715389a10c3aa900a4148c99e9
Marko Vračaričić <marko.vracaricic.83@gmail.com>
2024-10-09 06:21:36 +0200

    leaflet-must work on

--- script.js
+++ script.js
@@ -2,41 +2,20 @@ const toggler = document.querySelector('.navbar-toggler');
 const callNumber = document.querySelector('.disappear-large');
 const navbar = document.querySelector('.navbar-collapse');
 var switcher = true;
+var map = L.map('map').setView([44.8189, 20.4559], 15); // Promenjene koordinate na Dorćol, Beograd
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+  maxZoom: 18,
+  scrollWheelZoom: false,
+  doubleClickZoom: false ,
+}).addTo(map);
 // Dodavanje markera na Dorćol
 L.marker([44.8189, 20.4559]) 
   .addTo(map)
   .bindPopup('We are here.') 
   .openPopup();
 
-map.on('zoomend', function() {
-    if (map.getZoom() < 13) {
-        map.setZoom(13); // Vraća nivo uvećanja na minimum ako je manji
-    }
-});
-
-// Onemogućavanje pomeranja mape na dodir jednim prstom
-map.on('touchstart', function(e) {
-    if (e.touches.length === 1) {
-        e.preventDefault(); // Sprečava premestanje mape na dodir jednim prstom
-    }
-});
-
-// Onemogućavanje pomeranja mape kada je jedan prst
-map.on('touchmove', function(e) {
-    if (e.touches.length === 1) {
-        e.preventDefault(); // Sprečava pomeranje
-    } else if (e.touches.length > 1) {
-        // Omogućava pomeranje mape kada su korišćena dva ili više prstiju
-        map.dragging.enable();
-    }
-});
-
-// Omogućavanje mape za pomeranje samo sa dva prsta
-map.on('touchend', function(e) {
-    if (e.touches.length === 0) {
-        map.dragging.enable(); // Ponovno omogućava pomeranje nakon završetka dodira
-    }
-});
+ 
 
   toggler.addEventListener('click', () => {
   if (switcher) {
