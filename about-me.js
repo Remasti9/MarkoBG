3d69608e6593daf8140d1dca3c4a10212b0ea17c
Marko Vračaričić <marko.vracaricic.83@gmail.com>
2024-10-17 02:00:11 +0200

    changed design

--- about-me.js
+++ about-me.js
@@ -1,19 +1,23 @@
 const click = document.getElementById('about-me-btn');
+var aboutMe = document.querySelector('#about-us .container');
+var profileImg = document.getElementById('profile-image');
+var textAboutMe = document.getElementById('text-about-me');
 
 click.addEventListener('click', () => {
-    var aboutMe = document.getElementById('about-us');
-    var profileImg = document.getElementById('profile-image');
-    var textAboutMe = document.getElementById('text-about-me');
     if (!document.fullscreenElement) {
-        aboutMe.style.filter='saturate(1)'
-        setTimeout(()=>{
-            profileImg.style.transform='scale(1.5)';
-            textAboutMe.style.transform='scale(1.3)';
-            textAboutMe.style.marginLeft='100px';
-             profileImg.style.marginTop='-30px';
-             profileImg.style.filter='saturate(1)';
-        },800)
-        
+        aboutMe.style.filter = 'saturate(1)';
+        setTimeout(() => {
+            profileImg.style.transform = 'scale(1.5)';
+            textAboutMe.style.transform = 'scale(1.3)';
+            textAboutMe.style.marginLeft = '100px';
+            textAboutMe.style.color = 'white';
+            profileImg.style.marginTop = '-30px';
+            profileImg.style.filter = 'saturate(1)';
+            click.style.top = '-5px';
+            click.style.borderColor = 'white';
+            click.style.color = 'white';
+        }, 800);
+
         // Ako nije u full screen režimu, pokreni full screen
         if (aboutMe.requestFullscreen) {
             aboutMe.requestFullscreen();
@@ -24,23 +28,39 @@ click.addEventListener('click', () => {
         }
     } else {
         // Ako je već u full screen režimu, izađi iz njega
-        
-        profileImg.style.transform='scale(.7)';
-        textAboutMe.style.transform='scale(.7)';
-        textAboutMe.style.marginLeft='';
-        profileImg.style.marginTop='';
-        profileImg.style.filter='saturate(0)';
-        setTimeout(()=>{
-            aboutMe.style.filter='saturate(0)' ;
-            if (document.exitFullscreen) {
-                document.exitFullscreen();
-            } else if (document.webkitExitFullscreen) { /* Safari/Opera/Chrome */
-                document.webkitExitFullscreen();
-            } else if (document.msExitFullscreen) { /* IE/Edge */
-                document.msExitFullscreen();
-            }
-        },800)
-        
-        
+        exitFullscreen();
+    }
+});
+
+// Izlazak iz fullscreen režima
+function exitFullscreen() {
+    profileImg.style.transform = 'scale(.7)';
+    textAboutMe.style.transform = 'scale(.7)';
+    textAboutMe.style.marginLeft = '';
+    textAboutMe.style.color = 'black';
+    profileImg.style.marginTop = '';
+    profileImg.style.filter = 'saturate(0)';
+    click.style.top = '0';
+    click.style.borderColor = 'black';
+    click.style.color = 'black';
+    
+    setTimeout(() => {
+        aboutMe.style.filter = 'saturate(0)';
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.webkitExitFullscreen) { /* Safari/Opera/Chrome */
+            document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) { /* IE/Edge */
+            document.msExitFullscreen();
+        }
+    }, 800);
+}
+
+// Događaj za pritisak na dugme 'Esc'
+document.addEventListener('keydown', (event) => {
+    event.preventDefault()
+    console.log(event); // Debugging
+    if (event.key === 'Escape') {
+        exitFullscreen();
     }
 });
