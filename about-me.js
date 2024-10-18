4a1baf5d063de56c2f14e8d8e152cdea56790745
Marko Vračaričić <marko.vracaricic.83@gmail.com>
2024-10-18 02:57:18 +0200

    about-us goldenrod

--- about-me.js
+++ about-me.js
@@ -2,20 +2,25 @@ const click = document.getElementById('about-me-btn');
 var aboutMe = document.querySelector('#about-us .container');
 var profileImg = document.getElementById('profile-image');
 var textAboutMe = document.getElementById('text-about-me');
+var titleOfText = document.getElementById('title-of-text');
+var aboutUsTitle = document.querySelector('.about-us-title');
 
 click.addEventListener('click', () => {
     if (!document.fullscreenElement) {
         aboutMe.style.filter = 'saturate(1)';
         setTimeout(() => {
+            aboutUsTitle.style.color = 'goldenrod';
             profileImg.style.transform = 'scale(1.5)';
             textAboutMe.style.transform = 'scale(1.3)';
             textAboutMe.style.marginLeft = '100px';
-            textAboutMe.style.color = 'white';
+            textAboutMe.style.color = 'goldenrod';
+            textAboutMe.style.background = 'rgba(0,0,0,.5)';
+            titleOfText.style.color='goldenrod';
             profileImg.style.marginTop = '-30px';
             profileImg.style.filter = 'saturate(1)';
             click.style.top = '-5px';
-            click.style.borderColor = 'white';
-            click.style.color = 'white';
+            click.style.borderColor = 'goldenrod';
+            click.style.color = 'goldenrod';
             click.textContent='Povratak u glavni meni';
         }, 800);
 
@@ -44,11 +49,13 @@ click.addEventListener('click', () => {
 // Izlazak iz fullscreen režima
 function exitFullscreen() {
 
-
+        aboutUsTitle.style.color = 'white';
         profileImg.style.transform = 'scale(.7)';
         textAboutMe.style.transform = 'scale(.7)';
         textAboutMe.style.marginLeft = '';
         textAboutMe.style.color = 'black';
+        textAboutMe.style.background = 'rgba(255,255,255,.5)';
+        titleOfText.style.color='goldenrod';
         profileImg.style.marginTop = '';
         profileImg.style.filter = 'saturate(0)';
         click.style.top = '0';
