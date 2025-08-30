77c2998e648f44eaf3f90cd295e772aba2e27f91
Marko Vračaričić <marko.vracaricic.83@gmail.com>
2025-08-30 23:11:16 +0200

    solved main img

--- JS/img.js
+++ JS/img.js
@@ -8,13 +8,13 @@ const mainImg = document.getElementById('main-img');
 function setMainImg () {
   if(window.innerWidth>=576 && window.innerWidth <= 750){
     mainImg.src='images/750x750-template.png'
-     mainImg.style.aspectRatio='1/.7'
+     mainImg.style.aspectRatio='1/.6'
   } else if(window.innerWidth>=751 && window.innerWidth <=1000){
     mainImg.src='images/mainTabletMiddle.webp'
      mainImg.style.aspectRatio='1/.6'
   } else if (window.innerWidth>=1001 && window.innerWidth<=1199) {
    mainImg.src='images/1200.jpg'
-     mainImg.style.aspectRatio='1/.5'
+     mainImg.style.aspectRatio='1/.6'
   } 
   else if(window.innerWidth<=575) {
       mainImg.src='images/imgMobile2.png'
