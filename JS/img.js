2155a2b8205e4c18036b1552a0f686f1773b38dc
Marko Vračaričić <marko.vracaricic.83@gmail.com>
2025-09-10 15:37:29 +0200

    Tablet Img png

--- JS/img.js
+++ JS/img.js
@@ -7,13 +7,13 @@ const mainImg = document.getElementById('main-img');
 
 function setMainImg () {
   if(window.innerWidth>=576 && window.innerWidth <= 750){
-    mainImg.src='images/750x750-template.png'
+    mainImg.src='images/tabletImg.png'
      mainImg.style.aspectRatio='1/.6'
   } else if(window.innerWidth>=751 && window.innerWidth <=1000){
-    mainImg.src='images/mainTabletMiddle.webp'
+    mainImg.src='images/tabletImg.png'
      mainImg.style.aspectRatio='1/.6'
   } else if (window.innerWidth>=1001 && window.innerWidth<=1199) {
-   mainImg.src='images/1200.jpg'
+   mainImg.src='images/tabletImg.png'
      mainImg.style.aspectRatio='1/.6'
   } 
   else if(window.innerWidth<=575) {
